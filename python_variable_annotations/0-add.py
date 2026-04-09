2dfb092a4cf38dd15571e65ea98e2297ed4b40e9
Melopheelo12 <ryan12052002@gmail.com>
2026-04-09 09:27:33 +0200

    local save

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+<<<<<<< HEAD
 """Basic annotations"""
 
 
 def add(a: float, b: float) -> float:
     """Adds two floats and returns sum."""
+=======
+def add(a: float, b: float) -> float:
+    """Add two floats and return their sum."""
+>>>>>>> 42e0195 (local save)
     return a + b
