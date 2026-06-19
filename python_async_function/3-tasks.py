7a9b7ca7e3573ad134c00b38a47139aac142ecfd
Melopheelo12 <ryan12052002@gmail.com>
2026-06-19 11:11:18 +0200

    T3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -4,6 +4,7 @@ import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-def task_wait_random(max_delay : int):
+
+def task_wait_random(max_delay: int):
     attend = wait_random(max_delay)
     return asyncio.ensure_future(attend)
